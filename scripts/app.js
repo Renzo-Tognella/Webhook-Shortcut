46732f7d71794ef2a5c85c94364a2b0a5851747b
Renzo Tognella <122568128+Renzo-Tognella@users.noreply.github.com>
2024-09-23 12:56:38 -0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,91 +1,107 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const axios = require('axios/dist/node/axios.cjs'); 
+const axios = require('axios');
 
-app.get('/', (req, res) => {
-    if (req.method === "POST") {
-        let data = [];
+app.post('/', (req, res) => {
+    let data = [];
 
-        req.on("data", chunk => {
-            data.push(chunk);
-        });
+    req.on("data", chunk => {
+        data.push(chunk);
+    });
 
-        req.on("end", () => {
-            const body = Buffer.concat(data).toString();
-            const event = JSON.parse(body);
+    req.on("end", () => {
+        const body = Buffer.concat(data).toString();
+        const event = JSON.parse(body);
 
-            console.log("Evento Webhook recebido:", event);
+        console.log("Evento Webhook recebido:", event.actions[0].action);
 
-            if(event.actions.action == "update") {
-                verifyTypeOfEvent(event);
-            }
-            
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ status: 'success' }));
-        });
-    }
+        if (event.actions[0].action == "update") {
+            console.log("Entro");
+            verifyTypeOfEvent(event);
+        }
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ status: 'success' }));
+    });
 });
 
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
 
-function verifyTypeOfEvent(event) {
-    let payload = {}
-    let changes = event.actions.changes
+async function verifyTypeOfEvent(event) {
+    let changes = event.actions[0].changes
     let references = event.references
+    console.log(event.primary_id)
+    let idFreshService = await getCardInformations(event.primary_id)
+    let response
 
     if (changes.hasOwnProperty("started_at") && references[0].name == "To Do" && references[0].entity_type == "workflow-state") {
-        payload = EventToDo(event)
-        postEventWhenStateToDo(payload)
+        msg = "O desenvolvimento do seu card foi interrompido"
+        console.log(msg)
+        response = await postEvent(msg, idFreshService)
     }
     else if (changes.hasOwnProperty("started")) {
         if (changes.started.new == true && references[0].name == "In Progress" && references[0].entity_type == "workflow-state"){
-            payload = EventInProgress(event)
-            postEventWhenStateInProgress(payload)
+            msg = "O desenvolvimento do seu card foi iniciado"
+            console.log(msg)
+            response = await postEvent(msg, idFreshService)
         }
     }
     else if (changes.hasOwnProperty("completed")) {
         if (changes.completed.new == true && references[0].name == "Done" && references[0].entity_type == "workflow-state") {
-            payload = EventDone(event)
-            postEventWhenStateDone(payload)
+            msg = "O desenvolvimento do seu card foi concluído"
+            console.log(msg)
+            response = await postEvent(msg, idFreshService)
         }
+    }
+
+    console.log(response)
+
+}
+
+async function getCardInformations(args){
+    let link
+    try {
+        const response = await axios({
+            method: 'get',
+            url: `https://api.app.shortcut.com/api/v3/stories/${args}`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Shortcut-Token': '66e424d6-b301-4e5f-8f69-a1d5d1132c1a'
+            }
+        });
+
+        link = response.data.external_links[0].split("/tickets/")
+
+        return link[1]
+    } catch (error) {
+        console.error(error);
     }
 
 }
 
-function EventToDo(event) {
+async function postEvent(msg, idFreshService){
+    try {
+        let apiKey = "P6SN_I4t4C4zuPENzusZ"
+        const response = await axios({
+            method: 'post',
+            headers: {
+                'Authorization': `Basic ${Buffer.from(apiKey).toString('base64')}`,
+                'Content-Type': 'application/json'
+            },
+            url: `https://testrenzo.freshservice.com/api/v2/tickets/${idFreshService}/reply`,
+            data: {"body" : msg}
+        })
+        console.log(response)
 
+        const conversation = response.data.conversation;
+
+        return conversation.ticket_id;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-function EventInProgress(event) {
-
-}
-
-function EventDone(event) {
-
-}
-
-function getCardInformations(args){
-    axios({
-        method: 'get',
-        url: '',
-        responseType: 'stream'
-    })
-        .then(function (response) {
-            response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-        });
-
-}
-
-function postEventWhenStateDone(args){
-    
-}
-
-function postEventWhenStateInProgress(args){
-
-}
-function postEventWhenStateToDo(args){
-
-}
